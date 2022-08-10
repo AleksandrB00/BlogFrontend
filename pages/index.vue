e981f3badea73c5eb6e184e658709df52beb18ed
AleksandrB00 <babaev_aleksandr@icloud.com>
2022-08-10 17:27:57 +0200

    home, page_detail, aside

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,40 @@
 <template>
-  <h1>Привет, это главная страница</h1>
+  <div class="container">
+    <h1 class="my-3">Последние записи блога</h1>
+    <div class="row">
+      <div v-for="post in posts" :key="post.slug" class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img :src="post.image" alt="" class="card-img-top">
+            <div class="card-body">
+              <h4 class="card-title">{{ post.h1 }}</h4>
+              <div v-html="post.description" class="truncate"></div>
+              <div class="mb-2">
+              <span v-for="tag in post.tag">
+                <nuxt-link :to="`/tags/${tag}`" class="mr-1 badge badge-info">#{{ tag }}</nuxt-link>
+              </span>
+              </div>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <nuxt-link :to="`/posts/${post.slug}`" class="btn btn-sm btn-outline-secondary">Подробнее</nuxt-link>
+                </div>
+                <small class="text-muted">{{ post.post_date }}</small>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "index"
+  async asyncData(ctx) {
+    const { data } = await axios.get(`http://127.0.0.1:8000/blogapi/posts/`);
+    return {
+      posts: data.results,
+    }
+  }
 }
 </script>
 
---
+++ pages/posts/_slug.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <nav aria-label="breadcrumb" class="mt-4">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><nuxt-link to="/">Главная</nuxt-link></li>
+              <li class="breadcrumb-item active" aria-current="page">{{ post.h1 }}</li>
+            </ol>
+          </nav>
+          <img class="img-fluid rounded " :src="post.image" alt="">
+          <hr>
+          <h4 class="card-title">{{ post.h1 }}</h4>
+          <p v-html="post.text"></p>
+          <div class="d-flex justify-content-end">
+            <span v-for="tag in post.tags">
+                  <nuxt-link :to="`/tags/${tag}`" class="mr-1 badge badge-info">#{{ tag }}</nuxt-link>
+            </span>
+          </div>
+          <hr>
+          <div class="d-flex">
+            <div class="mr-auto p-2 lead">Автор: {{ post.author }}</div>
+            <div class="p-2">Опубликовано: {{ post.post_date }}</div>
+          </div>
+          <hr>
+          <Comments />
+        </div>
+        <Aside :tags=tags :aside=aside />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import post_detail from "@/layouts/post_detail";
+import Aside from "@/components/Aside";
+import Comments from "@/components/Comments";
+export default {
+  components: {
+    Aside,
+    Comments
+  },
+  layout: "post_detail",
+  async asyncData({params}) {
+    console.log(params)
+    const post = await axios.get(`http://127.0.0.1:8000/blogapi/posts/${params.slug}`);
+    const tags = await axios.get(`http://127.0.0.1:8000/blogapi/tags/`);
+    const aside = await axios.get(`http://127.0.0.1:8000/blogapi/aside/`);
+    return {
+      post: post.data,
+      tags: tags.data,
+      aside: aside.data,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
